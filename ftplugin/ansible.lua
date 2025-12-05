6efc3dbbf83733029af09a665dba223c67ab7a31
Gary Smith <gsmith12@wsgc.com>
2025-12-04 19:37:46 -0800

    Search in roles

--- ftplugin/ansible.lua
+++ ftplugin/ansible.lua
@@ -1,14 +1,25 @@
 local api = vim.api
-if vim.fn.executable('ansible-doc') then
-  vim.bo.keywordprg = ':sp term://ansible-doc'
+if vim.fn.executable("ansible-doc") then
+  vim.bo.keywordprg = ":sp term://ansible-doc"
 end
 local fname = api.nvim_buf_get_name(0)
-if fname:find('tasks/') then
-  local paths = {
-    vim.bo.path,
-    vim.fs.dirname(fname:gsub("tasks/", "files/")),
-    vim.fs.dirname(fname:gsub("tasks/", "templates/")),
-    vim.fs.dirname(fname)
-  }
+local paths = {
+  vim.bo.path,
+}
+if fname:find("tasks/") then
+  paths[#paths + 1] = vim.fs.dirname(fname:gsub("tasks/", "files/"))
+  paths[#paths + 1] = vim.fs.dirname(fname:gsub("tasks/", "templates/"))
+end
+if fname:find("roles/") then
+  paths[#paths + 1] = vim.fs.dirname(fname:gsub("roles/", "files/"))
+  paths[#paths + 1] = vim.fs.dirname(fname:gsub("roles/", "templates/"))
+end
+if #paths > 1 then
+  paths[#paths + 1] = vim.fs.dirname(fname)
+  vim.bo.path = table.concat(paths, ",")
+else
+  paths[#paths + 1] = "foo"
   vim.bo.path = table.concat(paths, ",")
 end
+
+vim.bo.suffixesadd = ".yaml,.yml,.j2,.jinja2"
